83499e785b03c6930397d4611a5f929d361ed7e8
Chris R <tratcher@outlook.com>
2018-01-06 21:07:26 -0800

    Minimal errors endpoint

---
+++ EDES/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDES.Controllers
+{
+    [Route("[controller]")]
+    public class ErrorController : Controller
+    {
+        // GET error
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
